5de41d78f8e4ef8b61581f349311bcd07e6019a7
Abhishek Kumar <abhishek.kr200802@gmail.com>
2017-01-23 19:59:54 +0530

    Add files via upload

---
+++ evalisp.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+'''
+EvaLisp is an evaluator for simple and limited Lisp expressions.
+'''
+
+from cin import cinput
+
+class ExpressionSyntaxError(Exception):
+    '''Exception raised when the user types wrong expressions.'''
+
+    def __init__(self, err=''):
+        Exception.__init__(self, err)
+
+def expression():
+    '''Evalute a Lisp expression.'''
+
+    cinput.skip_whitespace()
+    char = cinput.getc()
+    if char.isdigit() or char in {'-', '+'}:
+        cinput.ungetc(char)
+        num = cinput.getfloat()
+        return num
+    elif char == '(':
+        cinput.skip_whitespace()
+        operator = cinput.getc()
+        exp1 = expression()
+        exp2 = expression()
+        cinput.skip_whitespace()
+        close = cinput.getc()
+        if close != ')':
+            raise ExpressionSyntaxError('Missing ).')
+        return calc(operator, exp1, exp2)
+    else:
+        raise ExpressionSyntaxError('Unknown start of token, ' + char)
+
+def calc(operator, exp1, exp2):
+    '''Apply operator on exp1 and exp2.'''
+
+    if operator == '+':
+        return exp1 + exp2
+    elif operator == '-':
+        return exp2 - exp2
+    elif operator == '*':
+        return exp1 * exp2
+    elif operator == '/':
+        # let Python handle division by zero
+        return exp1 / exp2
+    else:
+        raise ExpressionSyntaxError('Unknow operator, ' + operator)
